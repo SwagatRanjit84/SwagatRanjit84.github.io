a587c871b43c5ad80b4fc03732f33272896f1424
Shruti Shrestha <shruti.shresthaa@gmail.com>
2022-02-09 13:28:30 +0545

    initial commit

---
+++ src/components/project.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+
+export default class Project extends Component {
+  render() {
+    return (
+      <div id="project">
+				<section className="colorlib-work" data-section="projects">
+					<div className="colorlib-narrow-content">
+						<div className="row">
+							<div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+								<span className="heading-meta">My Work</span>
+								<h2 className="colorlib-heading animate-box">Recent Projects</h2>
+							</div>
+						</div>
+						<div className="row">
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInLeft">
+								<div className="project" style={{backgroundImage: 'url(images/img-1.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Project 1</a></h3>
+											<span>Website</span>Project 1
+											<p className="icon">
+												<span><a href="#"><i className="icon-github" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInRight">
+								<div className="project" style={{backgroundImage: 'url(images/img-2.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Project 02</a></h3>
+											<span>Details</span>Project 2
+											<p className="icon">
+												<span><a href="#"><i className="icon-github" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInTop">
+								<div className="project" style={{backgroundImage: 'url(images/img-3.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Project 03</a></h3>
+											<span>Details</span>Project 3
+											<p className="icon">
+												<span><a href="#"><i className="icon-github" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="col-md-4 animate-box" data-animate-effect="fadeInBottom">
+								<div className="project" style={{backgroundImage: 'url(images/img-4.jpg)'}}>
+									<div className="desc">
+										<div className="con">
+											<h3><a href="work.html">Project 04</a></h3>
+											<span>Details</span>Project 4
+											<p className="icon">
+												<span><a href="#"><i className="icon-github" /></a></span>
+												<span><a href="#"><i className="icon-eye" /> 100</a></span>
+												<span><a href="#"><i className="icon-heart" /> 49</a></span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+							
+							
+						</div>
+					</div>
+				</section>
+      </div>
+    )
+  }
+}
